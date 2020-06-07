30b3c35770f64b322de5b6b66ea0da7f0476d01b
noebauerr <51676396+noebauerr@users.noreply.github.com>
2020-06-07 20:37:39 +0200

    Update neueDatei.ps1

--- neueDatei.ps1
+++ neueDatei.ps1
@@ -1,11 +1,22 @@
-# neue Datei vom 3.8. 8:35
+# neue Datei vom 3.8.2019
 
-# im Zuge vom HPI Kurs editiert
+# im Zuge vom HPI Kurs (juni 2020) editiert
 
 # git init             - erstellt ein neues lokales Repository
-# git add              - f�gt dateien hinzu, auch Wildcard m�glich
+# git add              - fügt dateien hinzu, auch Wildcard möglich
 # git commit -m 'info' - aktualisiert dann das Repository
 # git remove *.jpg     - entfernt dann Dateien
 
-# git remote ? ?       - l�dt ein lokales Repo ins INternet zB auf GitHub
-# git clone https:/github.com/noebauerr/repo repo - cloned das Github Repo auf die lokale maschine
+# git remote ? ?       - lädt ein lokales Repo ins Internet zB auf GitHub
+# git clone https:/github.com/noebauerr/repo repo - cloned das Github Repo auf ein lokales Repo
+
+# git push    - läft die Änderungen dann ins Online Repo bei Github
+
+# git fetch origin   - habe ich derzeit noch nicht probiert lädt die aktuelle version vom internet runter
+# git merge          - und verbindet es dann
+
+# git pull    - führt fetch und merge aus, beide Befehle in einem
+
+# git status
+
+# git config
