821b0df430186a7740c213f066947bcd1282c4ce
Reinhard Noebauer <reinhard@noebauer.net>
2020-06-07 19:58:47 +0200

    github datei bearbeitet wegen HPI kurs

--- neueDatei.ps1
+++ neueDatei.ps1
@@ -1,3 +1,11 @@
 # neue Datei vom 3.8. 8:35
-# bearbeitet online um 9:06
-# jetzt noch offline im vscode um 9:07 bearbeitet
+
+# im Zuge vom HPI Kurs editiert
+
+# git init             - erstellt ein neues lokales Repository
+# git add              - fügt dateien hinzu, auch Wildcard möglich
+# git commit -m 'info' - aktualisiert dann das Repository
+# git remove *.jpg     - entfernt dann Dateien
+
+# git remote ? ?       - lädt ein lokales Repo ins INternet zB auf GitHub
+# git clone https:/github.com/noebauerr/repo repo - cloned das Github Repo auf die lokale maschine
